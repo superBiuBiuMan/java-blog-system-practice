8938282b76b8d2d617dba49dbfb12d12505d1f66
superBiuBiu <zmqdream@qq.com>
2024-03-26 08:32:19 +0800

    项目初始化

---
+++ src/test/java/top/dreamlove/blog_system/BlogSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package top.dreamlove.blog_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
