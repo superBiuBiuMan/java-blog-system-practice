cdc1c7c414da2a29a09ff69ef24240ff89c1443b
superBiuBiu <zmqdream@qq.com>
2024-03-26 15:22:16 +0800

    ✨添加统一结果

--- src/main/java/top/dreamlove/blog_system/mapper/UserInfoMapper.java
+++ src/main/java/top/dreamlove/blog_system/mapper/UserInfoMapper.java
@@ -7,7 +7,6 @@ import top.dreamlove.blog_system.bean.UserInfo;
 // public interface UserInfoMapper extends BaseMapper<UserInfo> {
 // }
 
-@Mapper
 public interface UserInfoMapper  {
     //用户注册(需要将注册成功后的用户id返回)
 
